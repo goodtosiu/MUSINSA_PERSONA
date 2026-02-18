a090b3cc3a8ef52077ab37cb9b9f8ac772b7675c
siu <130466910+goodtosiu@users.noreply.github.com>
2026-02-18 15:02:17 +0900

    refactoring
    revert refactoring

--- backend/routes/static.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, send_from_directory
-from config import PROCESSED_DIR
-
-static_bp = Blueprint('static', __name__)
-
-@static_bp.route('/static/processed_imgs/<path:filename>')
-def serve_processed_image(filename):
-    return send_from_directory(PROCESSED_DIR, filename)
